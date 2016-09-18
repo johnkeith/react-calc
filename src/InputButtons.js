c40088cceddae875e91f17c94696f471eb38fbca
John Keith <johnkeith@outlook.com>
2016-09-18 16:28:34 -0400

    refactor input buttons

--- src/InputButtons.js
+++ src/InputButtons.js
@@ -2,17 +2,60 @@ import React, { Component } from 'react';
 
 class InputButtons extends Component {
   render() {
-    let buttons = [];
-    for(let i=1; i<=9; i++) {
-      buttons.push(<button key={`${i}-button`} onClick={() => this.props.addToCurrentInput(`${i}`)}>{i}</button>);
-      if(i % 3 === 0) {
-        buttons.push(<br key={`${i}-break`} />);
-      }
+    const buttons = {
+      firstRow: [
+        { type: 'action-button', value: 'AC', onClick: 'performAction' },
+        { type: 'action-button', value: '+/-', onClick: 'performAction' },
+        { type: 'action-button', value: '%', onClick: 'performAction' },
+        { type: 'action-button', value: '÷', onClick: 'setOperator' }
+      ],
+      secondRow: [
+        { type: 'number-button', value: '7', onClick: 'addToCurrentInput' },
+        { type: 'number-button', value: '8', onClick: 'addToCurrentInput' },
+        { type: 'number-button', value: '9', onClick: 'addToCurrentInput' },
+        { type: 'action-button', value: '*', onClick: 'setOperator' },
+      ],
+      thirdRow: [
+        { type: 'number-button', value: '4', onClick: 'addToCurrentInput' },
+        { type: 'number-button', value: '5', onClick: 'addToCurrentInput' },
+        { type: 'number-button', value: '6', onClick: 'addToCurrentInput' },
+        { type: 'action-button', value: '-', onClick: 'setOperator' },
+      ],
+      fourthRow: [
+        { type: 'number-button', value: '3', onClick: 'addToCurrentInput' },
+        { type: 'number-button', value: '2', onClick: 'addToCurrentInput' },
+        { type: 'number-button', value: '1', onClick: 'addToCurrentInput' },
+        { type: 'action-button', value: '+', onClick: 'setOperator' },
+      ],
+      fifthRow: [
+        { type: 'number-button', value: '0', onClick: 'addToCurrentInput' },
+        { type: 'number-button', value: '.', onClick: 'addToCurrentInput' },
+        { type: 'action-button', value: '=', onClick: 'calculate' },
+      ]
+    }  
+
+    let buttonsRendered = [];
+
+    for(const key in buttons) {
+      console.log(key);
+      buttons[key].forEach((row, index) => {
+        buttonsRendered.push(
+          <button key={`${index}-${row.type}-${row.value}`}
+            className={row.type}
+            onClick={() => this.props[row.onClick](row.value) }>
+            {row.value}
+          </button>
+        );
+
+        if(index !== 0 && index % 3 === 0) {
+          buttonsRendered.push(<br />)
+        }
+      });
     }
 
     return (
       <div>
-        {buttons}
+        {buttonsRendered}
       </div>
     );
   }
