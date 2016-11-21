3234f4ffd0d7cb24f293650f3e672bf3aaf43f8d
John Keith <johnkeith@outlook.com>
2016-11-21 17:37:58 -0500

    fix display of results to not include initial inputs

--- src/InputDisplay.js
+++ src/InputDisplay.js
@@ -3,25 +3,29 @@ import React, { PropTypes as T } from 'react';
 const InputDisplay = ({ beforeOperatorInput, afterOperatorInput, operatorInput, results }) => {
   const spacer = "\u00a0";
 
-  return (
-    <div className="input-display">
-      <span>
-        {beforeOperatorInput || spacer}
-      </span>
-      <span>
-        {` ${operatorInput || spacer} `}
-      </span>
-      <span> 
-        {` ${afterOperatorInput || spacer} `} 
-      </span>
-      <span>
-        { results ? " = " : spacer }
-      </span>
-      <span>
-        {` ${results || spacer}`}
-      </span>
-    </div>
-  );
+  let display = [];
+
+  if(results) {
+    display.push(<span key="results">{` ${results || spacer}` }</span>);
+  } 
+
+  if(!results && beforeOperatorInput) {
+    display.push(<span key="before-input">{beforeOperatorInput}</span>);
+  }
+
+  if(!results && operatorInput) {
+    display.push(<span key="operator-input"> {operatorInput} </span>);
+  }
+
+  if(!results && afterOperatorInput) {
+    display.push(<span key="after-input"> {afterOperatorInput} </span>);
+  }
+
+  if(!results && display.length === 0) {
+    display.push(<span key="spacer">{ spacer }</span>);
+  }
+
+  return (<div className="input-display">{ display }</div>);
 };
 
 // class InputDisplay extends Component {
